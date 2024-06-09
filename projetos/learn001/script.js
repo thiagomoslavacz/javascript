c0b961ec24a5f4d98c05116de51f36475bfd1c26
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-06-09 20:21:04 -0300

    Learning JavaScript

--- projetos/learn001/script.js
+++ projetos/learn001/script.js
@@ -1,1 +1,29 @@
+// while
+var c = 1;
+while (c <= 6) {
+  console.log(`Passo ${c}`);
+  c++;
+}
+
+console.log("------------");
+
+// do while
+var c = 1;
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 6);
+
+// for
+for (var c = 1; c <= 6; c++) {
+  console.log(c);
+}
+
+/*
 console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+*/
