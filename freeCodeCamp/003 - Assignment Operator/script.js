3509b8a39cbf44c2934410585449587039d80579
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-07-29 11:47:20 -0300

    Assignment Operator

---
+++ freeCodeCamp/003 - Assignment Operator/script.js
@@ -0,0 +1,27 @@
+// = (equals), é um operador de atribuição
+let a;
+let b = 5;
+
+console.log(a);
+
+a = 7; // 7 está sendo atribuído ao a
+
+b = a; // a está sendo atribuído ao b
+
+console.log(a);
+
+// -------------
+
+let c = 5;
+let d = 10;
+let e = "I am a ";
+
+c++;
+d = d + 5;
+e = e + "String!";
+
+// ------------
+
+// São diferentes:
+const camelCase = 1;
+const CAMELCASE = 1;
