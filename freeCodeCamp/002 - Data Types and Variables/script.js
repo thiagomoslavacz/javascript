20b347145f404ae430541b3553c91902ea458670
thiagomoslavacz <thiagomoslavacz@gmail.com>
2024-07-29 11:47:08 -0300

    Data Types and Variables

---
+++ freeCodeCamp/002 - Data Types and Variables/script.js
@@ -0,0 +1,11 @@
+/* Data Types:
+    undefined, null, boolean, string, symbol, number, object;
+*/
+
+var myName = "Thiago"; // Is not recommended
+
+let myOld = 14;
+
+myOld = 15; // Can be changed
+
+const pi = 3.14; // Cannot be changed
